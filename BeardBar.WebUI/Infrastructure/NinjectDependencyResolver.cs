61988fa89372798a968ccfebdd73e86b49219b4b
kuznetsova <atominkiss@gmail.com>
2021-04-04 19:53:31 +0300

    references and NinjectDependencyRelover.cs

---
+++ BeardBar.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BeardBar.WebUI.Infrastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel kernel;
+
+        public NinjectDependencyResolver(IKernel kernelParam)
+        {
+            kernel = kernelParam;
+            AddBindings();
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetService(Type serviceType)
+        {
+            return kernel.GetAll(serviceType)
+        }
+
+        private void AddBindings()
+        {
+            //Здесь будут привязки
+        }
+    }
+}
